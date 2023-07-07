91c6e074313f575bad031b04fc3457cf15e81b82
ARUL D <aruln311@gmail.com>
2023-07-07 21:06:46 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -32,20 +32,20 @@ const Header = () => {
     window.scrollTo(0, document.body.scrollHeight);
     setTimeout(() => {
       setShowArrow(false); // Hide the arrow after 3 seconds
-    }, 3000);
+    }, 2460);
   };
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  // const toggleDarkMode = () => {
+  //   setIsDarkMode(!isDarkMode);
+  // };
 
-  const toggleIcon = isSidebarOpen ? faTimes : faBars;
-  const modeIcon = isDarkMode ? faMoon : faLightbulb;
-  const modeText = isDarkMode ? 'Dark' : 'Light';
+  // const toggleIcon = isSidebarOpen ? faTimes : faBars;
+  // const modeIcon = isDarkMode ? faMoon : faLightbulb;
+  // const modeText = isDarkMode ? 'Dark' : 'Light';
 
   return (
     <>
-      <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
+      {/* <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
         <div className={`logo-container ${isScrolled ? 'show' : ''}`}>
           <div className="logo">Arul D</div>
         </div>
@@ -57,17 +57,73 @@ const Header = () => {
           )}
         </button>
         <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-          <ul>
+          <ul> */}
             {/* <li onClick={() => (window.location.href = '/')}>Home</li> */}
-            <li onClick={handleContactClick}>Contact</li>
+            {/* <li onClick={handleContactClick}>Contact</li> */}
             {/* <li className="dark-mode-toggle" onClick={toggleDarkMode}>
               <FontAwesomeIcon icon={modeIcon} className="mode-icon" />
               <span>{modeText}</span>
             </li> */}
-          </ul>
+          {/* </ul>
         </div>
       </header>
       {showArrow && (
+        <div className="arrow-container">
+          <img
+            src="https://genipluskids.com/wp-content/uploads/2021/11/Arrows-3-pointing-down-arrow-down-animated.gif"
+            alt="Arrow"
+            className="arrow-gif"
+          />
+        </div>
+      )} */}
+      
+      {isScrolled && (
+
+      <div data-testid="TopBar" id="TopBar" className="top-bar-container">
+      <button>
+        <div className="profile-picture-container logo">
+          <img
+            src="https://d1fdloi71mui9q.cloudfront.net/UEzg1DYSKWdB8HMhkfWg_88PSqm5yMladHUBA"
+            alt=""
+            role="presentation"
+            data-testid="TopBar-ProfileImage"
+            filter="none"
+            className="profile-picture"
+          /> 
+        <h3 className="logo1">Arul‎‎ D</h3></div> 
+      </button>
+      <div className="follow-button-container">
+        <div className="follow-button">
+          <button
+            data-testid="FollowButton"
+            className="follow-button-style"
+            id="follow-button-style"
+            onClick={handleContactClick}
+          >
+            <div className="follow-button-content">
+              <h4>Contact</h4>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+    )}
+      <div className="follow-button-container-fixed">
+        <div className="follow-button">
+          <button
+            data-testid="FollowButton"
+            className="follow-button-style"
+            id="follow-button-style"
+            onClick={handleContactClick}
+          >
+            <div className="follow-button-content">
+              <h4>Contact</h4>
+            </div>
+          </button>
+        </div>
+      </div>
+
+    {showArrow && (
         <div className="arrow-container">
           <img
             src="https://genipluskids.com/wp-content/uploads/2021/11/Arrows-3-pointing-down-arrow-down-animated.gif"
